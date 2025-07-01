18c62dd5a3f64ae50022103bea26c60c82229008
haris-hm <haris.mehuljic@drake.edu>
2025-07-01 18:07:47 -0500

    Initial Commit

---
+++ src/client/java/com/harismehuljic/wyvern/WyvernClient.java
@@ -0,0 +1,10 @@
+package com.harismehuljic.wyvern;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WyvernClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
