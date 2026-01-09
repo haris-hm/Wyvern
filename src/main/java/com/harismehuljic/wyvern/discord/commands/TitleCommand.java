0498d0c22af59d1082d08a7d05998666b029e24e
haris-hm <haris.mehuljic@drake.edu>
2026-01-09 14:31:46 -0600

    Add title and subtitle bolding and italicizing; finish implementing title length option

--- src/main/java/com/harismehuljic/wyvern/discord/commands/TitleCommand.java
+++ src/main/java/com/harismehuljic/wyvern/discord/commands/TitleCommand.java
@@ -7,6 +7,8 @@ import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
 import net.minecraft.network.packet.s2c.play.SubtitleS2CPacket;
 import net.minecraft.network.packet.s2c.play.TitleFadeS2CPacket;
 import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import reactor.core.publisher.Mono;
@@ -26,31 +28,61 @@ public class TitleCommand implements ApplicationCommand {
     @Override
     public Mono<Void> handle(ChatInputInteractionEvent event) {
         String subtitleText = "";
+
         String titleColor = "white";
         String subtitleColor = "white";
+
         int titleStayTicks = secondsToTicks(3);
+
+        boolean titleBolded = false;
+        boolean titleItalicized = false;
+        boolean subtitleBolded = false;
+        boolean subtitleItalicized = false;
 
         List<ApplicationCommandInteractionOption> options = event.getOptions();
 
         String titleText = event.getOption("title_text")
                 .flatMap(ApplicationCommandInteractionOption::getValue)
                 .map(ApplicationCommandInteractionOptionValue::asString)
-                .get();
+                .orElse("");
 
         for (ApplicationCommandInteractionOption option : options) {
-            if (option.getName().equals("subtitle_text")) {
-                subtitleText = option.getValue().map(ApplicationCommandInteractionOptionValue::asString).get();
+            switch (option.getName()) {
+                case "subtitle_text":
+                    subtitleText = option.getValue().map(ApplicationCommandInteractionOptionValue::asString).orElse("");
+                    break;
+                case "title_color":
+                    String titleColorProvided = option.getValue().map(ApplicationCommandInteractionOptionValue::asString).orElse("white");
+                    titleColor = minecraftColors.contains(titleColorProvided) ? titleColorProvided : titleColor;
+                    break;
+                case "subtitle_color":
+                    String subtitleColorProvided = option.getValue().map(ApplicationCommandInteractionOptionValue::asString).orElse("white");
+                    subtitleColor = minecraftColors.contains(subtitleColorProvided) ? subtitleColorProvided : titleColor;
+                    break;
+                case "title_bolded":
+                    titleBolded = option.getValue().map(ApplicationCommandInteractionOptionValue::asBoolean).orElse(false);
+                    break;
+                case "title_italicized":
+                    titleItalicized = option.getValue().map(ApplicationCommandInteractionOptionValue::asBoolean).orElse(false);
+                    break;
+                case "subtitle_bolded":
+                    subtitleBolded = option.getValue().map(ApplicationCommandInteractionOptionValue::asBoolean).orElse(false);
+                    break;
+                case "subtitle_italicized":
+                    subtitleItalicized = option.getValue().map(ApplicationCommandInteractionOptionValue::asBoolean).orElse(false);
+                    break;
+                case "length":
+                    int lengthProvided = Math.round(option.getValue().map(ApplicationCommandInteractionOptionValue::asLong).orElse(3L));
+                    if (lengthProvided > 15) lengthProvided = 15;
+                    titleStayTicks = secondsToTicks(lengthProvided);
+                    break;
             }
+        }
 
-            if (option.getName().equals("title_color")) {
-                String titleColorProvided = option.getValue().map(ApplicationCommandInteractionOptionValue::asString).get();
-                titleColor = minecraftColors.contains(titleColorProvided) ? titleColorProvided : titleColor;
-            }
-
-            if (option.getName().equals("subtitle_color")) {
-                String subtitleColorProvided = option.getValue().map(ApplicationCommandInteractionOptionValue::asString).get();
-                subtitleColor = minecraftColors.contains(subtitleColorProvided) ? subtitleColorProvided : titleColor;
-            }
+        if (titleText.isEmpty()) {
+            return event.reply()
+                    .withEphemeral(true)
+                    .withContent("Error: No title text provided.");
         }
 
         if (Wyvern.SERVER.getCurrentPlayerCount() == 0) {
@@ -59,21 +91,40 @@ public class TitleCommand implements ApplicationCommand {
                     .withContent("There are no players on the server.");
         }
 
+        MutableText titleDisplayText = Text.literal(titleText).formatted(Formatting.byName(titleColor));
+
+        if (titleBolded) titleDisplayText.formatted(Formatting.BOLD);
+        if (titleItalicized) titleDisplayText.formatted(Formatting.ITALIC);
+
         Wyvern.SERVER.getPlayerManager().sendToAll(new TitleFadeS2CPacket(secondsToTicks(1), titleStayTicks, secondsToTicks(1)));
 
         // Send a new title packet to all members
-        Wyvern.SERVER.getPlayerManager().sendToAll(new TitleS2CPacket(Text.literal(titleText).formatted(Formatting.byName(titleColor))));
+        Wyvern.SERVER.getPlayerManager().sendToAll(new TitleS2CPacket(titleDisplayText));
 
         if (!subtitleText.isEmpty()) {
-            Wyvern.SERVER.getPlayerManager().sendToAll(new SubtitleS2CPacket(Text.literal(subtitleText).formatted(Formatting.byName(subtitleColor))));
+            MutableText subtitleDisplayText = Text.literal(subtitleText).formatted(Formatting.byName(subtitleColor));
+            if (subtitleBolded) subtitleDisplayText.formatted(Formatting.BOLD);
+            if (subtitleItalicized) subtitleDisplayText.formatted(Formatting.ITALIC);
+
+            Wyvern.SERVER.getPlayerManager().sendToAll(new SubtitleS2CPacket(subtitleDisplayText));
+        }
+
+        String inGameMessage = String.format("@%s just displayed a title!", event.getUser().getUsername());
+
+        for (ServerPlayerEntity spe : Wyvern.SERVER.getPlayerManager().getPlayerList()) {
+            spe.sendMessage(Text.literal(inGameMessage).formatted(Formatting.AQUA));
         }
 
         return event.reply()
                 .withEphemeral(false)
-                .withContent(String.format("Title displayed successfully: ```\n%s\n%s\n```", titleText, subtitleText));
+                .withContent(String.format("Title displayed successfully for %d seconds: ```\n%s\n%s\n```", ticksToSeconds(titleStayTicks), titleText, subtitleText));
     }
 
     private int secondsToTicks(int seconds) {
         return seconds * ticksPerSecond;
+    }
+
+    private int ticksToSeconds(int ticks) {
+        return ticks / ticksPerSecond;
     }
 }
